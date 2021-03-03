1460a2882a22b09e81e897a6e4a0081b4e9417e7
Yvonne Farhani Armelia Arman <fonny.armelia@gmail.com>
2021-03-03 14:28:08 +0700

    Adjust and close buttons

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
 const popup = document.querySelector('.copy-container');
+const adjustButton = document.querySelectorAll('.adjust');
+const closeAdjustments = document.querySelectorAll(".close-adjustment");
+const sliderContainers = document.querySelectorAll('.sliders');
 let initialColors;
 
 //event listeners
@@ -27,6 +30,18 @@ popup.addEventListener('transitionend', () => {
     popup.classList.remove('active');
     popupBox.classList.remove('active');
 })
+
+adjustButton.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        openAdjustmentPanel(index);
+    });
+});
+
+closeAdjustments.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        closeAdjustmentPanel(index);
+    });
+});
 
 //functions
 //generate random hex code using math.random
@@ -170,6 +185,14 @@ function copyToClipboard(hex) {
     const popupBox = popup.children[0];
     popup.classList.add('active');
     popupBox.classList.add('active');
+}
+
+function openAdjustmentPanel(index) {
+    sliderContainers[index].classList.toggle('active');
+}
+
+function closeAdjustmentPanel(index) {
+    sliderContainers[index].classList.remove("active");
 }
 
 randomColors();
