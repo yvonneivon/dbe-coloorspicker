02113302f4671df2f080c44b7d0881c50d96f724
Yvonne Farhani Armelia Arman <fonny.armelia@gmail.com>
2021-03-04 14:03:11 +0700

    Completed

--- app.js
+++ app.js
@@ -265,7 +265,14 @@ function savePalette(e) {
         colors.push(hex.innerText);
     });
 
-    let paletteNr = savedPalettes.length;
+    let paletteNr;
+    const paletteObjects = JSON.parse(localStorage.getItem('palettes'));
+    if(paletteObjects){
+        paletteNr = paletteObjects.length;
+    } else {
+        paletteNr = savedPalettes.length;
+    }
+
     const paletteObj = {
         name,
         colors,
@@ -293,6 +300,21 @@ function savePalette(e) {
     paletteBtn.classList.add(paletteObj.nr);
     paletteBtn.innerText = 'Select';
 
+    //attach events to the buttons
+    paletteBtn.addEventListener('click', e => {
+        closeLibrary();
+        const paletteIndex = e.target.classList[1];
+        initialColors = [];
+        savedPalettes[paletteIndex].colors.forEach((color, index) => {
+            initialColors.push(color);
+            colorDivs[index].style.backgroundColor = color;
+            const text = colorDivs[index].children[0];
+            checkTextContrast(color, text);
+            updateTextUI(index);
+        });
+        resetInputs();
+    });
+
     //append to library
     palette.appendChild(title);
     palette.appendChild(preview);
@@ -303,7 +325,7 @@ function savePalette(e) {
 function savetoLocal(paletteObj) {
     let localPalettes;
     if (localStorage.getItem('palettes') === null) {
-        localPalettes = []
+        localPalettes = [];
     } else {
         localPalettes = JSON.parse(localStorage.getItem('palettes'));
     }
@@ -323,4 +345,54 @@ function closeLibrary() {
     popup.classList.remove('active');
 }
 
+function getLocal() {
+    if (localStorage.getItem('palettes') === null) {
+        localPalettes = [];
+    } else {
+        const paletteObjects = JSON.parse(localStorage.getItem('palettes'));
+        
+        savedPalettes = [...paletteObjects];
+        paletteObjects.forEach(paletteObj => {
+            //generate the palettes for library
+            const palette = document.createElement('div');
+            palette.classList.add('custom-palette');
+            const title = document.createElement('h4');
+            title.innerText = paletteObj.name;
+            const preview = document.createElement('div');
+            preview.classList.add('small-preview');
+            paletteObj.colors.forEach(smallColor => {
+                const smallDiv = document.createElement('div');
+                smallDiv.style.backgroundColor = smallColor;
+                preview.appendChild(smallDiv);
+            });
+            const paletteBtn = document.createElement('button');
+            paletteBtn.classList.add('pick-palette-btn');
+            paletteBtn.classList.add(paletteObj.nr);
+            paletteBtn.innerText = 'Select';
+
+            //attach events to the buttons
+            paletteBtn.addEventListener('click', e => {
+                closeLibrary();
+                const paletteIndex = e.target.classList[1];
+                initialColors = [];
+                savedPalettes[paletteIndex].colors.forEach((color, index) => {
+                    initialColors.push(color);
+                    colorDivs[index].style.backgroundColor = color;
+                    const text = colorDivs[index].children[0];
+                    checkTextContrast(color, text);
+                    updateTextUI(index);
+                });
+                resetInputs();
+            });
+
+            //append to library
+            palette.appendChild(title);
+            palette.appendChild(preview);
+            palette.appendChild(paletteBtn);
+            libraryContainer.children[0].appendChild(palette);
+        });
+    }
+}
+
+getLocal();
 randomColors();
